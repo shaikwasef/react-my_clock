2ed547e4ac2226f1fcfad16ac6e8f2b40c424eb0
shaikwasef <wasefshaik@gmail.com>
2021-02-19 17:29:46 +0530

    1 file modified

--- src/index.js
+++ src/index.js
@@ -4,40 +4,39 @@ import ReactDOM from "react-dom";
 import "./style.css";
 
 class Hour extends React.Component {
+  render(){
+    return (
+      <div id="hour" style ={{transform : this.props.hour_angle}}></div>
+    );
+  }
+
+}
+class Clock extends React.Component {
   constructor(props){
     super(props);
-    this.state = {hour_angle : ''};
+    this.state = {hour_angle : '' , minute_angle : '' , second_angle : '' };
     this.componentWillMount = this.componentWillMount.bind(this);
     this.tick = this.tick.bind(this);
   }
-
 
   componentWillMount(){
     this.timerId = setInterval(this.tick(),1000);
   }
 
   tick(){
-    const date = new Date();
+    const date = this.props.date ;
     const message = date.toLocaleTimeString().split(':');
     const hour_hand = parseInt(message[0]);
     const minute_hand = parseInt(message[1]);
     const x = hour_hand*30 + minute_hand*0.5;
-    this.setState ( {hour_angle : "rotate("+x+"deg)"})
+    this.setState({hour_angle : "rotate("+x+"deg)"})
   }
 
-  render(){
-    return (
-      <div id="hour" style ={{transform : this.state.hour_angle}}></div>
-    );
-  }
-
-}
-class Clock extends React.Component {
   render() {
     return (
     <div className = "outer_radius">
       <div className = "inner_radius">
-        <Hour />
+        <Hour hour_angle = {this.state.hour_angle} />
         <div id="minute"></div>
         <div id="second"></div>
         <div id= "dot"></div>
@@ -47,4 +46,4 @@ class Clock extends React.Component {
   }
 }
 
-ReactDOM.render(<Clock />, document.getElementById("root"));
+ReactDOM.render(<Clock date = {new Date()} />, document.getElementById("root"));
