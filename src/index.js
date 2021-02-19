13ac32852e4c74283e75234473826489d1dbb064
shaikwasef <wasefshaik@gmail.com>
2021-02-19 17:10:47 +0530

    2 files modified

--- src/index.js
+++ src/index.js
@@ -3,4 +3,42 @@ import ReactDOM from "react-dom";
 
 import "./style.css";
 
+class Hour extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {hour_angle : ''};
+    this.componentWillMount = this.componentWillMount.bind(this);
+    this.tick = this.tick.bind(this);
+  }
+
+
+  componentWillMount(){
+    this.timerId = setInterval(this.tick(),1000);
+  }
+
+  tick(){
+    const date = new Date();
+    const message = date.toLocaleTimeString().split(':');
+    const hour_hand = parseInt(message[0]);
+    const minute_hand = parseInt(message[1]);
+    const x = hour_hand*30 + minute_hand*0.5;
+    this.setState ( {hour_angle : "rotate("+x+"deg)"})
+  }
+
+}
+class Clock extends React.Component {
+  render() {
+    return (
+    <div class = "outer_radius">
+      <div class = "inner_radius">
+        <div id="hour"></div>
+        <div id="minute"></div>
+        <div id="second"></div>
+        <div id= "dot"></div>
+      </div>
+  </div>
+    );
+  }
+}
+
 ReactDOM.render(<Clock />, document.getElementById("root"));
